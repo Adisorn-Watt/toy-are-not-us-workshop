b17a32de5f8dd56465e51df449fe72bf90f81f54
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-07-30 15:37:12 +0700

    [Update] page-search.component

--- frontend/src/app/page-search/page-search.component.ts
+++ frontend/src/app/page-search/page-search.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-search.component.css']
 })
 export class PageSearchComponent implements OnInit {
+  inputSearch: string = "";
+  toyList: lorem = [];
 
   constructor() { }
 
