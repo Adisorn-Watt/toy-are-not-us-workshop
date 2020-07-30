20247c26ffc95e813284fae4b86ca6be720d5e05
phisonkha <phison.khankhang@allianz.com>
2020-07-30 15:19:20 +0700

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageSearchComponent } from './page-search/page-search.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ShippingAddressComponent } from './shipping-address/shipping-address.component';
+import { PagePaymentComponent } from './page-payment/page-payment.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: PageSearchComponent },
+  { path: 'search/:id', component: ProductDetailComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'shipping-address', component: ShippingAddressComponent },
+  { path: 'payment', component: PagePaymentComponent },
+  { path: 'order-success', component: OrderSuccessComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
