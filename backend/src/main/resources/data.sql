a55d80abc8d33cfc732357c203e467d3746257dd
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-08-07 15:28:49 +0700

    edit data.sql img

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,94 +1,94 @@
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-2, '43 Piece dinner Set', 'Female', '3 to 5',12.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+1, 2, '43 Piece dinner Set', 'Female', '3 to 5',12.95, true, 'CoolKidz', 'https://ae01.alicdn.com/kf/Hfcc46622c4fc44a28a7b6454a802b6df1.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-1, 'Balance Training Bicycle', 'Neutral', '3 to 5', 119.95, true, 'SportsFun');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+2, 1, 'Balance Training Bicycle', 'Neutral', '3 to 5', 119.95, true, 'SportsFun', 'https://i.pinimg.com/564x/82/ac/88/82ac88ce3a35deaac7943d168677b494.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-7, 'Best Friends Forever Magnetic Dress Up', 'Female', '3 to 5', 24.95, true,'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+3, 7, 'Best Friends Forever Magnetic Dress Up', 'Female', '3 to 5', 24.95, true,'CoolKidz', 'https://s.catch.com.au/images/product/0001/1519/5456eb62acc2f587821526.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-8, 'City Gargage Truck Lego', 'Neutral', '3 to 5', 19.95, false,'Lego' );
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+4, 8, 'City GargtoyAge Truck Lego', 'Neutral', '3 to 5', 19.95, false,'Lego' , 'https://images-na.ssl-images-amazon.com/images/I/81Dey7V1SyL._AC_SX425_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-20, 'Creator Beach House Lego', 'Neutral', '6 to 8', 39.95, false, 'Lego' );
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+5, 20, 'Creator Beach House Lego', 'Neutral', '6 to 8', 39.95, false, 'Lego' , 'https://images-na.ssl-images-amazon.com/images/I/51Ev8GhXGKL._AC_SX425_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-23, 'Dancing Alligator','Neutral', 'Baby', 19.95, true, 'CoolKidz' );
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+6, 23, 'Dancing Alligator','Neutral', 'Baby', 19.95, true, 'CoolKidz', 'https://images-na.ssl-images-amazon.com/images/I/41cBJR01WjL._AC_SX425_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-11, 'Earth DVD Game','Neutral', 'over 8', 34.99, true, 'VideoVroom' );
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+7, 11, 'Earth DVD Game','Neutral', 'over 8', 34.99, true, 'VideoVroom','https://images-na.ssl-images-amazon.com/images/I/51yrHu3VZcL._AC_SX425_.jpg' );
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-30, 'Fisher-Price stoller', 'Female', 'Toddler', 25.99, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+8, 30, 'Fisher-toyPrice stoller', 'Female', 'Toddler', 25.99, true, 'CoolKidz', 'https://images-na.ssl-images-amazon.com/images/I/81KtYbEHEZL._AC_SY550_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-19, 'Gourmet Cupcake Maker', 'Female', '6 to 8', 39.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+9, 19, 'Gourmet Cupcake Maker', 'Female', '6 to 8', 39.95, true, 'CoolKidz', 'https://images-na.ssl-images-amazon.com/images/I/61iBguprIIL._AC_SX425_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-4, 'Hoppity Ball 26 inch', 'Neutral', '3 to 5', 29.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+10, 4, 'Hoppity Ball 26 inch', 'Neutral', '3 to 5', 29.95, true, 'CoolKidz', 'https://images-na.ssl-images-amazon.com/images/I/51SmHf%2BiRnL._AC_SL1001_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-3, 'Horses and Unicorns Set', 'Neutral', '3 to 5', 24.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+11, 3, 'Horses and Unicorns Set', 'Neutral', '3 to 5', 24.95, true, 'CoolKidz', 'https://th-live-02.slatic.net/p/31155651f4f96f9ebd8d303b1520b62b.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-21, 'Jacques the peacock Play and Grow', 'Neutral', 'Toddler', 24.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+12, 21, 'Jacques the peacock Play and Grow', 'Neutral', 'Toddler', 24.95, true, 'CoolKidz', 'https://i.pinimg.com/originals/e5/18/19/e51819440bdf5939e3bfafe2ef873ce4.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-9,' Kettrick Tricycle','Neutral', '3 to 5', 249.95, false, 'SportsFun');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+13, 9,' Kettrick Tricycle','Neutral', '3 to 5', 249.95, false, 'SportsFun', 'https://images-na.ssl-images-amazon.com/images/I/81gvgm8c7nS._AC_SX425_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-26, 'Les Dollie Toffee Apple', 'Female', 'Toddler', 24.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+14, 26, 'Les Dollie Toffee Apple', 'Female', 'Toddler', 24.95, true, 'CoolKidz', 'https://images-na.ssl-images-amazon.com/images/I/91HAd3v1bxL._AC_SL1500_.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-24, 'Mashaka the Monkey', 'Neutral', 'Baby', 36.95, true, 'BarnyardBlast');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+15, 24, 'Mashaka the Monkey', 'Neutral', 'Baby', 36.95, true, 'BarnyardBlast', 'https://image.makewebeasy.net/makeweb/0/yWSGoz9KF/AMORBABY/IMG_2892_1040.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-28, 'Melody Express Musical Train', 'Male', 'Toddler', 42.95, true,'Modelz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+16, 28, 'Melody Express Musical Train', 'Male', 'Toddler', 42.95, true,'Modelz', 'https://i.ebayimg.com/images/g/RtUAAOSwCkxdGc7k/s-l1600.jpg');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-31, 'Mortimer the Moose Play and Grow', 'Neutral', 'Toddler', 12.95, true, 'CoolKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+17, 31, 'Mortimer the Moose Play and Grow', 'Neutral', 'Toddler', 12.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-29, 'My First LEGO DUPLO Set', 'Neutral', 'Toddler', 19.95, false, 'Lego');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+18, 29, 'My First LEGO DUPLO Set', 'Neutral', 'Toddler', 19.95, false, 'Lego');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 22, 'Nutbrown Hare', 'Neutral', 'Baby', 12.99, false, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 14, 'OMG - Gossip Girl Board Game', 'Female', 'over 8', 24.95, true, 'GeekToys');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 6, 'Princess Palace', 'Female', '3 to 5',  24.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 10, 'Princess Training Bicycle', 'Female', '3 to 5', 119.95, false, 'SportsFun');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 15, 'Sailboat', 'Male', 'over 8', 24.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 27, 'Sand Play Set', 'Neutral', 'Toddler', 24.95, true, 'Modelz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 16, 'Scrabble', 'Neutral', 'over 8', 19.95, true, 'GreekToys');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 13, 'Sattlers of Catan Board Game', 'Neutral', 'over 8', 44.95, true, 'GreekToys');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 25, 'Sleep Sheep', 'Neutral', 'Baby', 39.0, true, 'BarnyardBlast');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-5, 'Sleeping Queens Board Game', 'Female', '3 to 5', 12.95, false, 'CoolKids');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+5, 'Sleeping Queens Board Game', 'Female', '3 to 5', 12.95, false, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-18, 'Snoopy Sno-Cone Machine', 'Neutral', '6 to 8', 24.95, true, 'ModelKidz');
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+18, 'Snoopy Sno-Cone Machine', 'Neutral', '6 to 8', 24.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 17, 'Star Wars Darth Vadar Lego', 'Male', '6 to 8', 39.95, true, 'GreekToys');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, toyID, toyName, toyGender, toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
 12, 'Twilight Board Game', 'Neutral', 'over 8', 24.95, true, 'GreekToys');
 
 
