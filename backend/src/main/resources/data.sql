4bfddf8e048c60b69ff8ce1e7a05f4156b708676
chatchanok.wsa <chatchanok.wongsamang@allianz.com>
2020-08-07 15:18:07 +0700

    data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,50 +1,50 @@
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 2, '43 Piece dinner Set', 'Female', '3 to 5',12.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 1, 'Balance Training Bicycle', 'Neutral', '3 to 5', 119.95, true, 'SportsFun');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 7, 'Best Friends Forever Magnetic Dress Up', 'Female', '3 to 5', 24.95, true,'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-8, 'City Gargage Truck Lego', 'Neutral', '3 to 5', 19.95, false,'Lego' );
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
+8, 'City GargtoyAge Truck Lego', 'Neutral', '3 to 5', 19.95, false,'Lego' );
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 20, 'Creator Beach House Lego', 'Neutral', '6 to 8', 39.95, false, 'Lego' );
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 23, 'Dancing Alligator','Neutral', 'Baby', 19.95, true, 'CoolKidz' );
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 11, 'Earth DVD Game','Neutral', 'over 8', 34.99, true, 'VideoVroom' );
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-30, 'Fisher-Price stoller', 'Female', 'Toddler', 25.99, true, 'CoolKidz');
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
+30, 'Fisher-toyPrice stoller', 'Female', 'Toddler', 25.99, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 19, 'Gourmet Cupcake Maker','Female', '6 to 8', 39.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 4,'Hoppity Ball 26 inch','Neutral','3 to 5', 29.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE (
 3,'Horses and Unicorns Set','Neutral', '3 to 5', 24.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE(
 21,'Jacques the peacock Play and Grow','Neutral','Toddler', 24.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE(
 9,'Kettrick Tricycle','Neutral','3 to 5', 249.95, false, 'SportsFun');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE(
 26,'Les Dollie Toffee Apple', 'Female','Toddler', 24.95, true, 'CoolKidz');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+INSERT INTO toy (id, name, gender, toyAge, toyPrice, toyAvailable, toyBrand) VALUE(
 24,'Mashaka the Monkey', 'Neutral','Baby', 36.95, true, 'BarnyardBlast');
 
-INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-28,'Melody Express Musical Train','Male','Toddler', 42.95, true,'Modelz');
+INSERT INTO toy (id, toyID, toyName, toyGender,toyAge, toyPrice, toyAvailable, toyBrand, toyImage) VALUE(
+31 ,28,'Melody Express Musical Train','Male','Toddler', 42.95, true,'Modelz','https://ae01.alicdn.com/kf/Hfcc46622c4fc44a28a7b6454a802b6df1.jpg');
 
 
 
