f7fce61ec902a2d7b561d266d4daf73d6a04b209
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-08-07 14:47:42 +0700

    [completed] toy data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -23,28 +23,74 @@ INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
 30, 'Fisher-Price stoller', 'Female', 'Toddler', 25.99, true, 'CoolKidz');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-19, 'Gourmet Cupcake Maker','Female', '6 to 8', 39.95, true, 'CoolKidz');
+19, 'Gourmet Cupcake Maker', 'Female', '6 to 8', 39.95, true, 'CoolKidz');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-4,'Hoppity Ball 26 inch','Neutral','3 to 5', 29.95, true, 'CoolKidz');
+4, 'Hoppity Ball 26 inch', 'Neutral', '3 to 5', 29.95, true, 'CoolKidz');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE (
-3,'Horses and Unicorns Set','Neutral', '3 to 5', 24.95, true, 'CoolKidz');
+3, 'Horses and Unicorns Set', 'Neutral', '3 to 5', 24.95, true, 'CoolKidz');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-21,'Jacques the peacock Play and Grow','Neutral','Toddler', 24.95, true, 'CoolKidz');
+21, 'Jacques the peacock Play and Grow', 'Neutral', 'Toddler', 24.95, true, 'CoolKidz');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-9,'Kettrick Tricycle','Neutral','3 to 5', 249.95, false, 'SportsFun');
+9,' Kettrick Tricycle','Neutral', '3 to 5', 249.95, false, 'SportsFun');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-26,'Les Dollie Toffee Apple', 'Female','Toddler', 24.95, true, 'CoolKidz');
+26, 'Les Dollie Toffee Apple', 'Female', 'Toddler', 24.95, true, 'CoolKidz');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-24,'Mashaka the Monkey', 'Neutral','Baby', 36.95, true, 'BarnyardBlast');
+24, 'Mashaka the Monkey', 'Neutral', 'Baby', 36.95, true, 'BarnyardBlast');
 
 INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
-28,'Melody Express Musical Train','Male','Toddler', 42.95, true,'Modelz');
+28, 'Melody Express Musical Train', 'Male', 'Toddler', 42.95, true,'Modelz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+31, 'Mortimer the Moose Play and Grow', 'Neutral', 'Toddler', 12.95, true, 'CoolKidz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+29, 'My First LEGO DUPLO Set', 'Neutral', 'Toddler', 19.95, false, 'Lego');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+22, 'Nutbrown Hare', 'Neutral', 'Baby', 12.99, false, 'CoolKidz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+14, 'OMG - Gossip Girl Board Game', 'Female', 'over 8', 24.95, true, 'GeekToys');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+6, 'Princess Palace', 'Female', '3 to 5',  24.95, true, 'CoolKidz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+10, 'Princess Training Bicycle', 'Female', '3 to 5', 119.95, false, 'SportsFun');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+15, 'Sailboat', 'Male', 'over 8', 24.95, true, 'CoolKidz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+27, 'Sand Play Set', 'Neutral', 'Toddler', 24.95, true, 'Modelz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+16, 'Scrabble', 'Neutral', 'over 8', 19.95, true, 'GreekToys');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+13, 'Sattlers of Catan Board Game', 'Neutral', 'over 8', 44.95, true, 'GreekToys');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+25, 'Sleep Sheep', 'Neutral', 'Baby', 39.0, true, 'BarnyardBlast');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+5, 'Sleeping Queens Board Game', 'Female', '3 to 5', 12.95, false, 'CoolKids');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+18, 'Snoopy Sno-Cone Machine', 'Neutral', '6 to 8', 24.95, true, 'ModelKidz');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+17, 'Star Wars Darth Vadar Lego', 'Male', '6 to 8', 39.95, true, 'GreekToys');
+
+INSERT INTO toy (id, name, gender, age, price, availability, brand) VALUE(
+12, 'Twilight Board Game', 'Neutral', 'over 8', 24.95, true, 'GreekToys');
+
 
 
 
